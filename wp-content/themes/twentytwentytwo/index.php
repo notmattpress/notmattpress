612b5920e6f40e3b1f03d58e700c53adb1318305
Robert DeVore <deviodigital@gmail.com>
2024-10-06 23:20:44 -0400

    Add GitHub Actions workflow files to sync-branch

--- wp-content/themes/twentytwentytwo/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// There is nothing output here because block themes do not use php templates.
-// There is a core ticket discussing removing this requirement for block themes:
-// https://core.trac.notmatt.press/ticket/54272.
